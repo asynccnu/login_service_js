81b19ce2f341ba5649a9bb645b9f2e71f90e795e
neo1218 <834597629@qq.com>
2017-06-27 22:37:09 +0800

    done

--- src/login.js
+++ src/login.js
@@ -9,11 +9,9 @@ const querystring = require('querystring');
 const app = module.exports = new Koa()
 const router = require('koa-router')()
 
-login_url = 'http://portal.ccnu.edu.cn/loginAction.do'
-link_url = 'http://portal.ccnu.edu.cn/roamingAction.do?appId=XK'
-ticket_url1 = "http://122.204.187.6/hzsflogin?ticket=wKhQEg0HHcVx22CCW4Z7Y3POIK7HTT9YDDOH"
-ticket_url2 = 'http://122.204.187.6/xtgl/login_tickitLogin.html'
-menu_url = 'http://122.204.187.6/xtgl/index_initMenu.html?jsdm=xs&_t=1498483088016'
+var login_url = 'http://portal.ccnu.edu.cn/loginAction.do'
+var link_url = 'http://portal.ccnu.edu.cn/roamingAction.do?appId=XK'
+var ticket_url = 'http://122.204.187.6/xtgl/login_tickitLogin.html'
 
 // http basic authorization middleware
 app.use(async function(ctx, next) {
@@ -31,22 +29,16 @@ app.use(async function(ctx, next) {
             if (_cookies == undefined) {
                 ctx.status = 403
             }
-            console.log('_cookies:', _cookies)
             let __cookies = await link_login(_cookies)
             if (__cookies == undefined) {
                 ctx.status = 403
             }
-            console.log('__cookies:', __cookies)
-            let ___cookies = await ticket_login(__cookies, "POST", ticket_url2)
+            let ___cookies = await ticket_login(__cookies)
             if (___cookies == undefined) {
                 ctx.status = 403
             }
-            console.log('___cookies:', ___cookies) 
-            // let ____cookies = await ticket_login(___cookies, "POST", ticket_url2)
-            // if (___cookies == undefined) {
-            //     ctx.status = 403
-            // }
-            // console.log('____cookies:', ____cookies) // 现在这个cookie不对啊!!!
+            ctx.cookies = ___cookies[0] + ';' +  __cookies[1]
+            await next(ctx)
         }
     } catch (err) {
         throw err
@@ -59,7 +51,7 @@ async function info_login(sid, pwd) {
         headers: {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
             'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        },
     }
     var data = querystring.stringify({
         "userName": sid,
@@ -74,7 +66,7 @@ async function info_login(sid, pwd) {
         }
     })
     .catch (function (err) {
-        console.log(err)
+        throw err
     })
 }
 
@@ -86,19 +78,44 @@ async function link_login(cookies) {
         headers: {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
             "Cookie": request_cookies
+        },
+        maxRedirects: 0,
+        validateStatus: function (status) {
+            return status >= 200 && status < 303
         }
     }
     return axios.get(link_url, config)
+    .then (function (resp) {
+        let nexturl = resp.headers['location']
+        let cookies = redirect(nexturl)
+        return cookies
+    })
+    .catch (function (err) {
+        throw err
+    })
+}
+
+function redirect(nexturl) {
+    var config = {
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+        },
+        maxRedirects: 0,
+        validateStatus: function (status) {
+            return status >= 200 && status < 303
+        }
+    }
+    return axios.get(nexturl, config)
     .then (function (resp) {
         let cookies = resp.headers['set-cookie']
         return cookies
     })
     .catch (function (err) {
-        console.log(err)   
+        throw err
     })
 }
 
-async function ticket_login(cookies, method, ticket_url) {
+async function ticket_login(cookies) {
     JSSESIONID = cookies[0].split(';')[0]
     BIGipServerpool_jwc_xk = cookies[1].split(';')[0]
     request_cookies = JSSESIONID + ';' + BIGipServerpool_jwc_xk
@@ -108,29 +125,20 @@ async function ticket_login(cookies, method, ticket_url) {
             "Cookie": request_cookies,
             "Host":"122.204.187.6",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        },
+        maxRedirects: 0,
+        validateStatus: function (status) {
+            return status >= 200 && status < 303
         }
     }
-    if (method == "POST") {
-        return axios.post(ticket_url, config)
-        .then (function (resp) {
-            let cookies = resp.headers['set-cookie']
-            return cookies
-        })
-        .catch (function (err) {
-            console.log(err)
-        })
-    }
-    else if (method == "GET") {
-         return axios.get(ticket_url, config)
-        .then (function (resp) {
-            console.log(resp.headers)
-            let cookies = resp.headers['set-cookie']
-            return cookies
-        })
-        .catch (function (err) {
-            console.log(err)
-        })
-    }
+    return axios.get(ticket_url, config)
+    .then (function (resp) {
+        let cookies = resp.headers['set-cookie']
+        return cookies
+    })
+    .catch (function (err) {
+        throw err
+    })
 }
 
 // info login api
@@ -138,8 +146,10 @@ router.get('/api/info/login/', info_login_api)
 
 // api handlers
 async function info_login_api(ctx) {
-    // let cookies = await login_spider(sid, pwd)
-    ctx.body = "hello koa"
+    let JSSESIONID = ctx.cookies.split(';')[0].split('=')[1]
+    let BIGipServerpool_jwc_xk = ctx.cookies.split(';')[2].split('=')[1]
+    ctx.type = 'json'
+    ctx.body = '{"JSSESIONID":"'+JSSESIONID+'",'+'"BIGipServerpool_jwc_xk":"'+BIGipServerpool_jwc_xk+'"}'
 }
 
 app.use(router.routes(), router.allowedMethods());
